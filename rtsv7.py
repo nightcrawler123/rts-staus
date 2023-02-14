49e18a6583885c869cfcd662dc2b37dc4dcb881f
nightcrawler123 <nightcrawler123@users.noreply.github.com>
2023-02-14 17:50:13 +0530

    Update rtsv7.py

--- rtsv7.py
+++ rtsv7.py
@@ -12,13 +12,15 @@ output_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 output_file = f"ping-results_{output_date}.csv" # Add the output date and time to the output file name
 
 def ping(ip):
-    response = subprocess.run(['ping', '-n', '1', '-w', '1000', ip], capture_output=True) # Send one ping packet and wait up to 1 second for a response
+    response = subprocess.run(['ping', '-n', '1', '-w', '1000', '-l', '32', ip], capture_output=True) # Send one ping packet with 32 bytes and wait up to 1 second for a response
     stdout = response.stdout.decode().strip()
     stderr = response.stderr.decode().strip()
     if "Reply from " in stdout:
         status = "Online"
     elif "could not find host" in stderr:
-        status = "Host not found"
+        status = "Bad hostname"
+    elif "Request timed out" in stdout:
+        status = "Request timeout"
     else:
         status = "Offline"
     return {'IP': ip, 'Status': status}
@@ -28,6 +30,9 @@ start_time = time.monotonic()
 with concurrent.futures.ThreadPoolExecutor() as executor:
     results = list(executor.map(ping, ips))
 
+end_time = time.monotonic()
+script_runtime = end_time - start_time
+
 with open(output_file, 'w', newline='') as csv_file:
     fieldnames = ['IP', 'Status']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -35,6 +40,4 @@ with open(output_file, 'w', newline='') as csv_file:
     for result in results:
         writer.writerow(result)
 
-end_time = time.monotonic()
-
-print(f"Script completed in {end_time - start_time:.2f} seconds.")
+print(f"Script runtime: {script_runtime:.2f} seconds")
