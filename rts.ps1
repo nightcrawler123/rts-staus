01363a9028b060a3f67e4fce0e4efa7062eacb91
nightcrawler123 <nightcrawler123@users.noreply.github.com>
2023-04-06 07:58:57 +0530

    Update rts.ps1

--- rts.ps1
+++ rts.ps1
@@ -16,3 +16,20 @@ foreach ($ip in $ips) {
 }
 
 $results | Export-Csv -Path $outputFile -NoTypeInformation
+
+
+
+
+Sub SplitEachWorksheet()
+Dim FPath As String
+FPath = Application.ActiveWorkbook.Path
+Application.ScreenUpdating = False
+Application.DisplayAlerts = False
+For Each ws In ThisWorkbook.Sheets
+    ws.Copy
+    Application.ActiveWorkbook.SaveAs Filename:=FPath & "\" & ws.Name & ".xlsx"
+    Application.ActiveWorkbook.Close False
+Next
+Application.DisplayAlerts = True
+Application.ScreenUpdating = True
+End Sub
